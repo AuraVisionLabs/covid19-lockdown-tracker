5ab8d2c26c8360b2aae5015bb0635c7b510825cb
Daniel Martinho-Corbishley <Dan0net>
2020-04-01 10:59:46 +0100

    April 1st update

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import pycountry
 import flag
 import re
 pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.width', 500)
 
 import requests
 from bs4 import BeautifulSoup
@@ -30,9 +32,62 @@ remove_sup(china_table)
 
 df_inter = pd.read_html(str(inter_table))[0]
 df_inter = df_inter.iloc[:-1]
+
+df_inter = df_inter.append(pd.DataFrame([
+    {'Country': 'United States', 'Place': 'Alaska', 'Start date': '2020-03-28', 'End date': '2020-04-11', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Arizona', 'Start date': '2020-03-31', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'California', 'Start date': '2020-03-19', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Colorado', 'Start date': '2020-03-26', 'End date': '2020-04-11', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Connecticut', 'Start date': '2020-03-23', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Delaware', 'Start date': '2020-03-24', 'End date': '2020-05-15', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'District of Columbia', 'Start date': '2020-04-01', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Hawaii', 'Start date': '2020-03-25', 'End date': '2020-04-30', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Idaho', 'Start date': '2020-03-25', 'End date': '2020-04-15', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Illinois', 'Start date': '2020-03-21', 'End date': '2020-04-07', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Indiana', 'Start date': '2020-03-24', 'End date': '2020-04-06', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Kansas', 'Start date': '2020-03-30', 'End date': '2020-04-19', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Louisiana', 'Start date': '2020-03-23', 'End date': '2020-04-12', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Maryland', 'Start date': '2020-03-30', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Massachusetts', 'Start date': '2020-03-24', 'End date': '2020-04-07', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Michigan', 'Start date': '2020-03-24', 'End date': '2020-04-14', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Minnesota', 'Start date': '2020-03-27', 'End date': '2020-04-10', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Montana', 'Start date': '2020-03-28', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'New Hampshire', 'Start date': '2020-03-27', 'End date': '2020-05-04', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'New Jersey', 'Start date': '2020-03-21', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'New Mexico', 'Start date': '2020-03-24', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'New York', 'Start date': '2020-03-22', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'North Carolina', 'Start date': '2020-03-30', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Ohio', 'Start date': '2020-03-23', 'End date': '2020-04-06', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Oregon', 'Start date': '2020-03-23', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Rhode Island', 'Start date': '2020-03-30', 'End date': '2020-04-26', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Virginia', 'Start date': '2020-03-30', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Vermont', 'Start date': '2020-03-25', 'End date': '2020-04-15', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Washington', 'Start date': '2020-03-25', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'West Virginia', 'Start date': '2020-03-24', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Vermont', 'Start date': '2020-03-25', 'End date': '2020-04-15', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Wisconsin', 'Start date': '2020-03-25', 'End date': '2020-04-24', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Vermont', 'Start date': '2020-03-25', 'End date': '2020-04-15', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Maryland', 'Start date': '2020-03-23', 'End date': '2020-04-07', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Nevada', 'Start date': '2020-03-17', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Virginia', 'Start date': '2020-03-24', 'End date': '2020-04-23', 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Kentucky', 'Start date': '2020-03-23', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Georgia', 'Start date': '2020-03-23', 'End date': np.nan, 'Level': 'State'},
+    {'Country': 'United States', 'Place': 'Oklahoma', 'Start date': '2020-03-25', 'End date': '2020-04-30', 'Level': 'State'},
+    {'Country': 'Lithuania', 'Place': 'Lithuania', 'Start date': '2020-03-16', 'End date': '2020-04-13', 'Level': 'National'},
+    {'Country': 'United Arab Emirates', 'Place': 'United Arab Emirates', 'Start date': '2020-03-22', 'End date': '2020-04-05', 'Level': 'National'},
+    {'Country': 'Lebanon', 'Place': 'Lebanon', 'Start date': '2020-03-22', 'End date': '2020-04-12', 'Level': 'National'},
+    {'Country': 'Kuwait', 'Place': 'Kuwait', 'Start date': '2020-03-14', 'End date': np.nan, 'Level': 'National'},
+    {'Country': 'India', 'Place': 'India', 'Start date': '2020-03-24', 'End date': '2020-04-14', 'Level': 'National'},
+    {'Country': 'Peru', 'Place': 'Peru', 'Start date': '2020-03-16', 'End date': '2020-04-12', 'Level': 'National'},
+    {'Country': 'Canada', 'Place': 'Quebec', 'Start date': '2020-03-13', 'End date': np.nan, 'Level': 'Province'},
+    {'Country': 'El Salvador', 'Place': 'El Salvador', 'Start date': '2020-03-22', 'End date': np.nan, 'Level': 'National'},
+    {'Country': 'Czech Republic', 'Place': 'Czech Republic', 'Start date': '2020-03-22', 'End date': '2020-04-12',
+     'Level': 'National'}
+]), ignore_index=True)
+df_inter = df_inter.drop_duplicates(['Country', 'Place'], keep='last')
 df_inter['Confirmed'] = ''
 
-df_china = pd.read_html(str(china_table))[0]
+df_china = pd.read_html(str(china_table), header=0)[0]
 df_china = df_china.iloc[1:-1]
 df_china = df_china[['Place', 'Start date', 'End date']]
 df_china['Country'] = 'China'
@@ -42,50 +97,52 @@ df_china['Confirmed'] = 'Confirmed'
 df_quar = pd.concat((df_inter, df_china))
 df_quar.loc[df_quar['Place'] == df_quar['Country'], 'Place'] = np.nan
 
-print(df_quar)
-df_quar.to_csv('quarantine_dates.csv')
+df_quar.to_csv('deploy/lockdown_dates.csv', index=False)
+df_quar.to_csv('history/lockdown_dates_%s.csv' % (pd.datetime.now().strftime('%d-%m-%y')), index=False)
+df_quar_old = pd.read_csv('history/lockdown_dates_%s.csv' % ((pd.datetime.now() - pd.offsets.Day(1)).strftime('%d-%m-%y')))
+
+print(pd.concat([df_quar, df_quar_old]).drop_duplicates(keep=False).sort_values(['Country', 'Place']))
 
 df_quar['Start'] = pd.to_datetime(df_quar['Start date'], format='%Y-%m-%d')
 df_quar['Finish'] = pd.to_datetime(df_quar['End date'], format='%Y-%m-%d')
 
-df_quar.loc[df_quar['Finish'] > (pd.datetime.now() + pd.offsets.Day(60)), 'Finish'] = np.nan
+df_quar_china_us = df_quar[df_quar['Country'].isin(['United States', 'China'])]
+df_quar_other = df_quar[~df_quar['Country'].isin(['United States', 'China'])]
+df_quar_other = df_quar_other.groupby('Country').agg({'Start': 'min', 'Finish': 'max', 'Place': lambda x: ', '.join(x.fillna('')), 'Level': 'first'}).reset_index()
+# print(df_quar_other)
+df_quar = pd.concat((df_quar_china_us, df_quar_other))
 
+df_quar.loc[df_quar['Finish'] > (pd.datetime.now() + pd.offsets.Day(60)), 'Finish'] = np.nan
 df_quar['Duration'] = df_quar['Finish'] - df_quar['Start']
+df_quar['Name'] = np.where(df_quar['Level'] == 'National', df_quar['Country'], df_quar['Country'] + ' (' + df_quar['Place'] + ')')
+df_quar['CC'] = df_quar['Country'].apply(lambda x: pycountry.countries.search_fuzzy(x)[0].alpha_2)
+df_quar['CCE'] = df_quar['CC'].apply(lambda x: flag.flag(x))
+df_quar['Complete'] = df_quar['Finish'] <= pd.datetime.now()
+print(df_quar)
+df_quar = df_quar.sort_values(['Start', 'Country', 'Duration'], ascending=[False, False, True]).reset_index()
+
 average_confirmed_days = df_quar[df_quar['Confirmed'] == 'Confirmed']['Duration'].mean().days
 average_review_days = df_quar[df_quar['Confirmed'] != 'Confirmed']['Duration'].mean().days
 max_confirmed_days = df_quar[df_quar['Confirmed'] == 'Confirmed']['Duration'].max().days
 average_to_review_days = (df_quar[df_quar['Confirmed'] != 'Confirmed']['Finish'] - pd.datetime.now()).mean().days
 average_into_lockdown_days = (pd.datetime.now() - df_quar[df_quar['Confirmed'] != 'Confirmed']['Start']).mean().days
-df_quar['Complete'] = df_quar['Finish'] <= pd.datetime.now()
-total_finished = (df_quar[df_quar['Confirmed'] == 'Confirmed'].groupby('Country')['Complete'].mean() == 1.0).sum()
+total_finished_country = df_quar[(df_quar['Confirmed'] == 'Confirmed') & (df_quar['Complete'])]['Country'].nunique()
+total_finished = len(df_quar[(df_quar['Confirmed'] == 'Confirmed') & (df_quar['Complete'])])
+no_end = df_quar['Finish'].isna().sum()
 
-df_quar_wuh = df_quar[df_quar['Place'] == 'Wuhan'].iloc[0]
-# df_quar = df_quar.groupby('Country').agg({'Start': 'min', 'Finish': 'max', 'Confirmed': 'max'}).reset_index()
-# df_quar['Duration'] = df_quar['Finish'] - df_quar['Start']
-
-df_quar['Name'] = df_quar['Country'] + (' (' + df_quar['Place'] + ')').fillna('')
-# df_quar['Name'] = df_quar['Country']
-df_quar = df_quar.sort_values('Start', ascending=False).reset_index()
-df_quar['CC'] = df_quar['Country'].apply(lambda x: pycountry.countries.search_fuzzy(x)[0].alpha_2)
-df_quar['CCE'] = df_quar['CC'].apply(lambda x: flag.flag(x))
-print(df_quar.head())
-
-x_points = [df_quar_wuh['Start'], df_quar_wuh['Start'] + pd.offsets.Day(21), df_quar_wuh['Start'] + pd.offsets.Day(42),
-            df_quar_wuh['Start'] + pd.offsets.Day(average_confirmed_days),
-            df_quar_wuh['Start'] + pd.offsets.Day(max_confirmed_days)]
 x_points = [0, 1, 2, 3, 4]
-x_text = ['<b>Lockdowns started<br>in %d Countries</b><br>%d Days ago️<br>on average' % (
+x_text = ['<b>Lockdowns started</b><br>%d in %d Countries<br>%d Days ago️<br>on average' % ( len(df_quar),
     df_quar['Country'].nunique(), average_into_lockdown_days),
           '<b>Lockdown reviews</b><br>%d Days from now<br>on average' % (average_review_days),
-          '<b>Lockdowns ended<br>in %d Countries</b><br>so far' % (total_finished),
+          '<b>Lockdowns ended</b><br>%d in %d Countries<br>so far' % (total_finished, total_finished_country),
           '<b>Average duration</b><br>%d Days confirmed<br>so far' % (average_confirmed_days),
           '<b>Maximum duration</b><br>%d Days confirmed<br>so far' % (max_confirmed_days)]
 x_color = ['rgb(255,0,90)', 'rgb(255,165,0)', 'rgb(0,255,165)', 'rgb(223,223,223)', 'rgb(180,180,180)']
-print(x_points)
 line_height = -30
 info_height = 0
 offset_height = -100
 
+print(x_text, no_end)
 
 def time_fmt(row, date, text):
     dtt = (date - pd.datetime.now()).days + 1
@@ -215,7 +272,7 @@ fig2 = go.Figure(
                  mode='lines',
                  line={
                      'width': 3,
-                     'color': x_color[3] if row['Finish'] <= pd.datetime.now() and row['Confirmed'] == 'Confirmed' else x_color[0],
+                     'color': x_color[2] if row['Finish'] <= pd.datetime.now() and row['Confirmed'] == 'Confirmed' else x_color[0],
                  },
                  hoverinfo='none',
                  xaxis='x2',
@@ -283,7 +340,7 @@ fig2 = go.Figure(
         'hovermode': 'closest',
         'paper_bgcolor': 'rgba(255,255,255)',
         'plot_bgcolor': 'rgba(0,0,0,0)',
-        'height': 2200,
+        'height': 2400,
         'margin': {'t': 40, 'l': 0, 'r': 0, 'b': 0},
         'annotations': [
                            {
@@ -297,7 +354,7 @@ fig2 = go.Figure(
                                'yanchor': 'top',
                                'align': 'left',
                                'text': '<b>Last updated ' + pd.datetime.now().strftime(
-                                   '%d %B %Y') + '</b><br>How past and current lockdowns are unfolding using data collated by <a href="https://en.wikipedia.org/wiki/National_responses_to_the_2019%E2%80%9320_coronavirus_pandemic#In_other_countries">Wikipedia</a> contributors, updated daily.<br>Please share if you find this useful. For an interactive version please visit <a href="https://auravision.ai/covid19-lockdown-tracker">https://auravision.ai/covid19-lockdown-tracker</a>.',
+                                   '%d %B %Y') + '</b><br>The most comprehensive source for how past and current lockdowns are unfolding, updated daily. Please share if you find this useful.<br>Interactive version and data download <a href="https://auravision.ai/covid19-lockdown-tracker">https://auravision.ai/covid19-lockdown-tracker</a>.',
                            },
                            {
                                'x': 1.0, 'y': 0, 'xref': 'paper', 'yref': 'paper',
@@ -310,7 +367,7 @@ fig2 = go.Figure(
                                'xanchor': 'right',
                                'yanchor': 'bottom',
                                'align': 'right',
-                               'text': 'For an interactive version, please visit <a href="https://auravision.ai/covid19-lockdown-tracker">https://auravision.ai/covid19-lockdown-tracker</a>.<br>Data from <a href="https://en.wikipedia.org/wiki/National_responses_to_the_2019%E2%80%9320_coronavirus_pandemic#In_other_countries">https://en.wikipedia.org/wiki/National_responses_to_the_2019-20_coronavirus_pandemic#In_other_countries</a>.<br>Aura Vision is a provider of in-store retail analytics, for more information please visit <a href="https://auravision.ai">https://auravision.ai</a>.'
+                               'text': 'Data sources include: <a href="https://en.wikipedia.org/wiki/National_responses_to_the_2019%E2%80%9320_coronavirus_pandemic#In_other_countries">https://en.wikipedia.org/wiki/National_responses_to_the_2019-20_coronavirus_pandemic#In_other_countries</a>,<br><a href="https://www.businessinsider.com/countries-on-lockdown-coronavirus-italy-2020-3?r=US&IR=T">https://www.businessinsider.com/countries-on-lockdown-coronavirus-italy-2020-3?r=US&IR=T</a><br><a href="https://edition.cnn.com/2020/03/23/us/coronavirus-which-states-stay-at-home-order-trnd/index.html">https://edition.cnn.com/2020/03/23/us/coronavirus-which-states-stay-at-home-order-trnd/index.html</a>.<br><b>Aura Vision is a provider of in-store retail analytics, for more information please visit <a href="https://auravision.ai">https://auravision.ai</a></b>.'
                            }
                        ] + [
                            {
@@ -331,21 +388,13 @@ fig2 = go.Figure(
                                'text': 'Latest update '
                            }
                        ] + [{
-            'x': row['Start'], 'y': index * line_height + offset_height, 'xref': 'x2', 'yref': 'y2',
+            'x': row['Start'], 'y': index * line_height + offset_height + 5, 'xref': 'x2', 'yref': 'y2',
             'showarrow': False,
-            'font': {'size': 15},
+            'font': {'size': 13},
             'xanchor': 'right',
             'yanchor': 'middle',
 
-            'text': '<b>' + row['Name'] + '</b>          ' if row['Level'] == 'National' else row['Name'] + '          '
-        } for index, row in df_quar.iterrows()] + [{
-            'x': row['Start'], 'y': index * line_height + offset_height, 'xref': 'x2', 'yref': 'y2',
-            'showarrow': False,
-            'font': {'size': 22},
-            'xanchor': 'right',
-            'yanchor': 'middle',
-
-            'text': row['CCE'] + '  '
+            'text': ('<b>' + row['Name'] + '</b>' if row['Level'] == 'National' else row['Name']) + ' <span style="font-size: 18px">' + row['CCE'] + '</span>   ' + row['Start'].strftime('%d %b') + '  '
         } for index, row in df_quar.iterrows()],
         'xaxis': {
             'visible': False,
@@ -376,7 +425,7 @@ fig2 = go.Figure(
             'showgrid': False,
             'zeroline': True,
             'showticklabels': False,
-            'range': [len(df_quar) * line_height + offset_height - 40, 0],
+            'range': [len(df_quar) * line_height + offset_height, 0],
             'zerolinewidth': 1,
             'zerolinecolor': 'rgb(128,128,128)',
             'fixedrange': True
